7db563f6079fae165a36e06c8158f001609a40d9
BurakTetikk <burktetk@gmail.com>
2025-02-02 22:45:16 +0300

    WebDriverManager.chromedriver().setup();

---
+++ src/test/java/lesson_01/C01_FirstClass.java
@@ -0,0 +1,22 @@
+package lesson_01;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class C01_FirstClass {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.com");
+
+        driver.manage().window().maximize();
+
+        Thread.sleep(2000);
+
+        driver.close();
+    }
+}
