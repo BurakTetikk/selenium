0b1faa41dc6e9363aad7416c4712b4ced331a6c8
BurakTetikk <burktetk@gmail.com>
2025-02-04 01:07:56 +0300

    JUnit

---
+++ src/test/java/lesson_05/C01_JUnit.java
@@ -0,0 +1,70 @@
+package lesson_05;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C01_JUnit {
+
+    @Test
+    public void test01() {
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        driver.get("https://www.amazon.com");
+
+        WebElement searchBar = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
+
+        searchBar.sendKeys("Nutella");
+
+        searchBar.submit();
+
+        WebElement txt = driver.findElement(By.xpath("//h2[@class='a-size-base a-spacing-small a-spacing-top-small a-text-normal']"));
+
+        System.out.println(txt.getText());
+
+        driver.close();
+
+    }
+
+    @Test
+    public void test2(){
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        driver.get("https://www.bestbuy.com");
+
+        driver.close();
+    }
+
+
+    @Test
+    public void test3(){
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        driver.get("https://www.facebook.com");
+
+        driver.close();
+    }
+}
---
+++ src/test/java/lesson_05/C02_BeforeAfterAnnotations.java
@@ -0,0 +1,49 @@
+package lesson_05;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C02_BeforeAfterAnnotations {
+    WebDriver driver;
+
+
+    @Before
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+
+        driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+    }
+
+    @After
+    public void tearDown(){
+        driver.close();
+    }
+
+    @Test @Ignore
+    public void test01(){
+        driver.get("https://www.amazon.com");
+    }
+
+    @Test @Ignore
+    public void test02(){
+        driver.get("https://www.bestbuy.com");
+        System.out.println("Hi");
+    }
+
+    @Test
+    public void test03(){
+        driver.get("https://www.facebook.com");
+    }
+}
